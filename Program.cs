f036dc9c49b10f59a1200ccd2a16d80344eb0c2f
Sergey <aseev-1977@list.ru>
2022-12-23 17:57:13 +0300

    Задача 4

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿ Console.WriteLine("Введите 3 числа:");
+            int num_1 = int.Parse(Console.ReadLine());
+            int num_2 = int.Parse(Console.ReadLine());
+            int num_3 = int.Parse(Console.ReadLine());
+
+            int max = num_1;
+
+            while (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            while (num_3 > max)
+            {
+                max = num_3;
+            }
+
+            Console.WriteLine("Максимальное число -> " + max);
